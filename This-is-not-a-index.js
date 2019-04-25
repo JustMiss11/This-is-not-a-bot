67864181806b2a5542cee0c90222581d1a2be81e
JustMiss11 <47405121+JustMiss11@users.noreply.github.com>
2019-04-25 18:28:58 +0200

    Create This-is-not-a-index.js

---
+++ This-is-not-a-index.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js')
+      discord = require('discord.js')
+      disc = require('discord.js')
+      Disc = require('discord.js');
+      
+const this = new Discord.Client();
+
+this.login("NTcxMDA5NjIxOTE0MDI1OTk0.XMHgMg.xj6PJkhNLUcbMHIKAf02diLzsg0");
