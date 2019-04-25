c088d73ef419d554cb8a7d4a63716e4f2a9da046
JustMiss11 <47405121+JustMiss11@users.noreply.github.com>
2019-04-25 18:42:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const Discord = require('discord.js'),
       disc = require('discord.js'),
       Disc = require('discord.js');
       
-const this = new Discord.Client();
+const not = new Discord.Client();
 
-this.on("ready", () => {
-      this.log(this.user.username)
-      this.log(`is in ${this.guilds.size} servers`)
-      this.log(`with ${this.users.size} users`)
-      this.log(`${this.user.username} is online!`);
+not.on("ready", () => {
+      not.log(not.user.username)
+      not.log(`is in ${not.guilds.size} servers`)
+      not.log(`with ${not.users.size} users`)
+      not.log(`${not.user.username} is online!`);
 })
 
-this.login("NTcxMDA5NjIxOTE0MDI1OTk0.XMHgMg.xj6PJkhNLUcbMHIKAf02diLzsg0");
+not.login("NTcxMDA5NjIxOTE0MDI1OTk0.XMHgMg.xj6PJkhNLUcbMHIKAf02diLzsg0");
