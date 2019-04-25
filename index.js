cf1fe68e44fa71d74b3ccb0c53c2321c8bf2a8ff
JustMiss11 <47405121+JustMiss11@users.noreply.github.com>
2019-04-25 19:35:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,4 +16,34 @@ This.on("ready", () => {
       console.log("Made by JustNela (FEDE)");
 })
 
+This.on("message", async (message, msg) => {
+      let prf = "+";
+      let messageArray = message.content.split(" "),
+          cmd = messageArray[0],
+          args = messageArray.slice(1);
+      
+      if(!message.content.startsWith(prf)) return;
+      if(message.channel.dm) return message.reply("It wont work in DM's dummy..") && This.channels.get("571024698209599488").send({embed:{
+            "author": {
+                  "name": "Someone DMed to me"
+            },
+            "fields": [
+                  {
+                        "name": "User:",
+                        "value": message.author.username
+                  },
+                  {
+                        "name": "ID",
+                        "value": message.author.id
+                  },
+                  {
+                        "name": "content",
+                        "value": message.content
+                  }
+            ],
+            "color": 0x36393f
+      }})
+      
+})
+
 This.login("NTcxMDA5NjIxOTE0MDI1OTk0.XMHgMg.xj6PJkhNLUcbMHIKAf02diLzsg0");
