7103a061bbca41ba796c880de609f60a50c6b306
JustMiss11 <47405121+JustMiss11@users.noreply.github.com>
2019-04-25 18:57:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ const Discord = require('discord.js'),
       disc = require('discord.js'),
       Disc = require('discord.js');
       
-const not = new Discord.Client();
+const This = new Discord.Client();
 
-not.on("ready", () => {
-      not.log(not.user.username)
-      not.log(`is in ${not.guilds.size} servers`)
-      not.log(`with ${not.users.size} users`)
-      not.log(`${not.user.username} is online!`)
-      not.log("---------------------------------")
-      not.log("Made by JustNela (FEDE)");
+//let c = console;
+
+This.on("ready", () => {
+      console.log(This.user.username)
+      console.log(`is in ${This.guilds.size} servers`)
+      console.log(`with ${This.users.size} users`)
+      console.log(`${This.user.username} is online!`)
+      console.log("---------------------------------")
+      console.log("Made by JustNela (FEDE)");
 })
 
-not.login("NTcxMDA5NjIxOTE0MDI1OTk0.XMHgMg.xj6PJkhNLUcbMHIKAf02diLzsg0");
+This.login("NTcxMDA5NjIxOTE0MDI1OTk0.XMHgMg.xj6PJkhNLUcbMHIKAf02diLzsg0");
