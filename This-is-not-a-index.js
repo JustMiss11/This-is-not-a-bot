6097e008c01a65d6a44b11699dc9d94c3e257cc0
JustMiss11 <47405121+JustMiss11@users.noreply.github.com>
2019-04-25 18:32:00 +0200

    Update This-is-not-a-index.js

--- This-is-not-a-index.js
+++ This-is-not-a-index.js
@@ -1,8 +1,15 @@
-const Discord = require('discord.js')
-      discord = require('discord.js')
-      disc = require('discord.js')
+const Discord = require('discord.js'),
+      discord = require('discord.js'),
+      disc = require('discord.js'),
       Disc = require('discord.js');
       
 const this = new Discord.Client();
+
+this.on("ready", () => {
+      this.log(this.user.username)
+      this.log(`is in ${this.guilds.size} servers`)
+      this.log(`with ${this.users.size} users`)
+      this.log(`${this.user.username} is online!`);
+})
 
 this.login("NTcxMDA5NjIxOTE0MDI1OTk0.XMHgMg.xj6PJkhNLUcbMHIKAf02diLzsg0");
