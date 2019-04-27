36699c6482003f13eede9b1995490c89c9f32923
JustMiss11 <47405121+JustMiss11@users.noreply.github.com>
2019-04-27 23:24:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -68,8 +68,13 @@ server.on("message", async (message, msg) => { //reload;
             .setTitle("This is not a help")
             .setColor("0x36393f")
             .setTimestamp()
+            .addField("say", "Say command will say what you want to say xd")
             .setDescription("`help` - Viewing.");
             message.channel.send(emb)
+            return;
+      }
+      if(cmd === `${prf}say`){
+            message.channel.send(args.join(" "));
             return;
       }
 })
