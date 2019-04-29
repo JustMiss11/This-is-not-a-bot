7beab9b4601680ae1b072ad8232cdf9d3c56a792
JustMiss11 <47405121+JustMiss11@users.noreply.github.com>
2019-04-29 17:13:31 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,1 +1,15 @@
+const discord = require('discord.js');
 
+module.exports.run = async (server, message, args) => {
+  var emb = new disc.RichEmbed()
+            .setTitle("This is not a help")
+            .setColor("0x36393f")
+            .setTimestamp()
+            .addField("say", "Say command will say what you want to say xd")
+            .setDescription("`help` - Viewing.");
+            message.channel.send(emb)
+            return;
+}
+module.exports.help = {
+  name: "help"
+}
