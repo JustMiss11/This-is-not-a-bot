af3090738493b5302927e91679ae33acaabe241e
JustMiss11 <47405121+JustMiss11@users.noreply.github.com>
2019-04-29 17:14:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -88,16 +88,16 @@ server.on("message", async (message, msg) => { //reload;
       
      // if(!message.content.startsWith(prf)) return;
       
-      if(cmd === `${prf}help`){
-            var emb = new disc.RichEmbed()
-            .setTitle("This is not a help")
-            .setColor("0x36393f")
-            .setTimestamp()
-            .addField("say", "Say command will say what you want to say xd")
-            .setDescription("`help` - Viewing.");
-            message.channel.send(emb)
-            return;
-      }
+      //if(cmd === `${prf}help`){
+            //var emb = new disc.RichEmbed()
+           // .setTitle("This is not a help")
+           // .setColor("0x36393f")
+           // .setTimestamp()
+           // .addField("say", "Say command will say what you want to say xd")
+          //  .setDescription("`help` - Viewing.");
+           // message.channel.send(emb)
+           // return;
+     // }
       if(cmd === `${prf}say`){
             message.channel.send(args.join(" "));
             return;
