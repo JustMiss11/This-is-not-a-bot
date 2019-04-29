fe0401b53cd00da8c770c1bf64365968c4753488
JustMiss11 <47405121+JustMiss11@users.noreply.github.com>
2019-04-29 17:26:11 +0200

    Create say.js

---
+++ commands/say.js
@@ -0,0 +1,9 @@
+const discord = require('discord.js')
+
+module.exports.run = async (bot, message, args) => {
+    message.channel.send(args.join(" "));
+    return;
+}
+module.exports.help = {
+    name: "say"
+}
