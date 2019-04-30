e28c512dcbd7a1fc2bf7bbc6270a2d866126bd0a
JustMiss11 <47405121+JustMiss11@users.noreply.github.com>
2019-04-30 23:26:28 +0200

    Create set-prf.js

---
+++ commands/set-prf.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js'),
+      db = require('quick.db'),
+      colors = require('../colors.json');
+
+module.exports.run = async (server, message, args) => {
+  
+  let prf = args.join(" ");
+  db.push(`inv_${message.guild.id}`, prf)
+  if(!prf) return message.reply("You have to set a prefix.");
+  
+  if(!message.author.hasPermissions("MANAGE_SERVER")) return message.channel.send("Not enought permissions!");
+  
+  var semb = new Discord.RichEmbed()
+  .setAuthor("Succesful!")
+  .setDescription(`You have succesfully changed server prefix to ${prf}!`)
+  .setColor("GREEN");
+  message.channel.send(semb)
+  
+}
+
+module.exports.run = {
+ "name": "set-prefix"
+}
