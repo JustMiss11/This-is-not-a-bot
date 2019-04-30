6d3d6a5309716d0035c1af354a80cc5b09b2c7db
JustMiss11 <47405121+JustMiss11@users.noreply.github.com>
2019-04-30 23:49:23 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,13 +2,15 @@ const discord = require('discord.js');
 
 module.exports.run = async (server, message, args) => {
   var emb = new discord.RichEmbed()
-            .setTitle("This is not a help")
-            .setColor("0x36393f")
-            .setTimestamp()
-            .addField("say", "Say command will say what you want to say xd")
-            .setDescription("`help` - Viewing.");
-            message.channel.send(emb)
-            return;
+  .setTitle("This is not a help")
+  .setColor("0x36393f")
+  .setTimestamp()
+  .addField("say", "Say command will say what you want to say xd")
+  .addField("set-prefix", "Sets bots prefix. (needs to be set every update due to hosting. & can be used by admins)")
+  .addField("report-bug", "Reports bug to the support server")
+  .setDescription("`help` - Viewing.");
+  message.channel.send(emb)
+  return;
 }
 module.exports.help = {
   name: "help"
