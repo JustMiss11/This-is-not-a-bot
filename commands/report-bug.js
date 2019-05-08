23b3beeccc5f4bf2fa8875687a43a9a694b80e05
JustMiss11 <47405121+JustMiss11@users.noreply.github.com>
2019-05-08 16:47:22 +0200

    Update report-bug.js

--- commands/report-bug.js
+++ commands/report-bug.js
@@ -13,7 +13,34 @@ module.exports.run = async (server, message, args) => {
   .addField("User reporting bug:", user)
   .addField("Bug:", bug)
   .setColor(colors.disc);
-  server.channels.get('572892675553099801').send(remb);
+  message.channel.send("Succesful!")
+  server.channels.get('572892675553099801').send(remb).then(msg => {
+    msg.react('✅').then(() => msg.react('❌'));
+
+            const filter = (reaction, user) => {
+                 return ['✅', '❌'].includes(reaction.emoji.name) && user.id === message.author.id;
+            };
+
+   message.awaitReactions(filter, { max: 1, time: 600000000000, errors: ['time'] })
+	.then(collected => {
+		const reaction = collected.first();
+
+		if (reaction.emoji.name === '✅') {
+      msg.edit({embed: {
+        "author": "Bug will be repaired in shortest time.",
+        "color": 0x36393f,
+        
+      }});
+		}
+    if (reaction.emoji.name === '❌'){
+      msg.edit("Bug have been already repaired/is not a bug.");
+		}
+	})
+	.catch(collected => {
+		console.log(`After a minute, only ${collected.size} out of 4 reacted.`);
+		message.reply('you didn\'t react with neither a thumbs up, nor a thumbs down.');
+	});
+  })
   return;
   
 }
