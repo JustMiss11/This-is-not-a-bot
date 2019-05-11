71b5567cacdfb848a2bdde9ceb649985e0ff71fb
JustMiss11 <47405121+JustMiss11@users.noreply.github.com>
2019-05-11 19:15:46 +0200

    Update answers.js

--- commands/answers.js
+++ commands/answers.js
@@ -11,8 +11,12 @@ module.exports.run = async (server, message, args) => {
       message.channel.send("You have enabled general answers. (example: Whyy! Cuz your mom gay!)")
       return;
    }
+   if(args[0] == "disable"){
+         const answ = db.set(`asw_${message.guild.id}`, 0);
+         message.channel.send("Turned off general answers");
+   }
    if(answ === 1){
-         if(message.content.startsWith("why") || message.content.startsWith("Why")){message.reply("Cuz ur mom gay!")}
+         if(message.content.startsWith("why") || message.content.startsWith("Why"))return message.reply("Cuz ur mom gay!");
    }
 }
 
