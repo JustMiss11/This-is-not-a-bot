e0905ce3c96cd2715b7db7a33ac06201d56cf7a8
JustMiss11 <47405121+JustMiss11@users.noreply.github.com>
2019-05-13 15:14:25 +0200

    Create balance.js

---
+++ commands/balance.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js'),
+      db = require('quick.db');
+      
+module.exports.run = async (server, message, args) => {
+      const bal = db.fetch(`bal_${message.author.id}`);
+      if(bal == null) bal = 0;
+      
+      var bemb = Discord.RichEmbed()
+      .setAuthor("Your Balance", message.author.avatarURL)
+      .setColor("0xE99C38")
+      .addField("BETA", "-BETA TESTING-");
+      message.channel.send(bemb);
+      return;
+}
+module.exports.help = {
+  name: "balance"
+}
