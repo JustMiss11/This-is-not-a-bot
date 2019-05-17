5eed79e1de902f234242fc59c2c8b9e3c9cf62a2
JustMiss11 <47405121+JustMiss11@users.noreply.github.com>
2019-05-17 16:22:25 +0200

    Create buy.js

---
+++ commands/buy.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js'),
+      db = requir3('quick.db');
+
+module.exports.run = async (server, message, args) => {
+      let bal = db.fetch(`userBalance_${message.author.id}`);
+      if(args[0] == "badge"){
+      
+            if(args[1] == "nitro"){
+               if(bal <= 5000).then(message.channel.send("Not enought money to purchase this") return;
+               
+               db.subtract(`userBalance_${message.author.id}`, 5000).then(i => {
+               var embed = new Discord.RichEmbed()
+               .setAuthor("Successfully bought Nitro badge!")
+               .setColor("GREEN")
+               .setDescription("Now you have Nitro Badge in your profile!");
+               message.channel.send(embed)
+            }
+      }
+}
+module.exports.help = {
+  "name": "buy"
+}
