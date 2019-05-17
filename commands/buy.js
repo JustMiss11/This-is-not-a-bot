d1cfebd8eb323c26e90946fa4bbdfde5a9c85718
JustMiss11 <47405121+JustMiss11@users.noreply.github.com>
2019-05-17 16:59:56 +0200

    Update buy.js

--- commands/buy.js
+++ commands/buy.js
@@ -12,6 +12,7 @@ module.exports.run = async (server, message, args) => {
                }else{
                
                      db.subtract(`userBalance_${message.author.id}`, 5000)
+                     db.push(`badges_€{message.author.id}`, <:nitro:578957860843814912>)
                      var embed = new Discord.RichEmbed()
                      .setAuthor("Successfully bought Nitro badge!")
                
