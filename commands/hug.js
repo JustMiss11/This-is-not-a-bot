c38beb71c4d153d9b291f6a8f752ec4be6b7fd69
JustMiss11 <47405121+JustMiss11@users.noreply.github.com>
2019-05-17 22:03:08 +0200

    Hugs for special moments :)

---
+++ commands/hug.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js'),
+      db = require('quick.db');
+module.exports.run = async (server, message, args) => {
+      var user = message.mentions.members.first();
+      if(!user) return message.reply("is alone he wants some hug :)");
+      if(user.id === message.author.id) return message.reply("is feeling alone he wants some hugs ;)");
+      db.add(`hugsG_${message.author.id}`, 1)
+      db.add(`hugsT_${user.id}`, 1)
+      var embed = new Discord.RichEmbed()
+      .setAuthor(message.author.username)
+      .setColor("GREEN")
+      .addField(`Hugged`, user.tag)
+      .addField("Its a nice moment :)");
+      message.channel.send(emb);
+      return;
+}
