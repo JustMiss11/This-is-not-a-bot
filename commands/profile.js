ee7ca9c66c681f64bc29724fcadd539dd18bbb37
JustMiss11 <47405121+JustMiss11@users.noreply.github.com>
2019-05-17 15:42:18 +0200

    Update profile.js

--- commands/profile.js
+++ commands/profile.js
@@ -4,20 +4,24 @@ const Discord = require('discord.js'),
 module.exports.run = async (server, message, args) => {
       let user = message.mentions.members.first() || message.author;
       var userB = await db.fetch(`userBalance_${user.id}`);
+      var stat = await db.fetch(`status_${user.id}`);
       if(userB === null) userB = 0;
+      
       
       if(user.id !== message.author.id){
             var emb = new Discord.RichEmbed()
-            .setAuthor(`{user.username}'s profile.`)
+            .setAuthor(`${user.username}'s profile.`)
             .setColor("BLUE")
-            .addField("Balance:", `€{userB}`);
+            .addField("Status:", `${stat ? `${stat}` : 'I dont want you to see this :) (This is default status'}`)
+            .addField("Balance:", `€${userB}`);
             message.channel.send(emb)
             return;
       }else{
             var emb = new Discord.RichEmbed()
             .setAuthor("You'r profile")
             .setColor("BLUE")
-            .addField("Balance:", `€${userB}`);
+            .addField("Status:", `${stat ? `${stat}` : 'I dont want you to see this :) (This is default status)'}`
+            .addField("Balance:", `€${userB}`, true);
             message.channel.send(emb)
             return;
       }
