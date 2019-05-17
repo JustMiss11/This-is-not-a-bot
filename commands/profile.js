7ebd925141d7a7b7d7cc534ea38f1c8b0746014d
JustMiss11 <47405121+JustMiss11@users.noreply.github.com>
2019-05-17 15:26:13 +0200

    Update profile.js

--- commands/profile.js
+++ commands/profile.js
@@ -2,13 +2,20 @@ const Discord = require('discord.js'),
       db = require('quick.db');
 
 module.exports.run = async (server, message, args) => {
-      let user = message.guild.mentions.members.first() || message.author;
+      let user = message.mentions.members.first() || message.author;
       var userB = await db.fetch(`userBalance_${user.id}`);
       if(userB === null) userB = 0;
       
-      if(user.id = message.author.id){
+      if(user.id !== message.author.id){
             var emb = new Discord.RichEmbed()
-            .setAuthor("You'r profile.")
+            .setAuthor(`{user.username}'s profile.`)
+            .setColor("BLUE")
+            .addField("Balance:", `€{userB}`);
+            message.channel.send(emb)
+            return;
+      }else{
+            var emb = new Discord.RichEmbed()
+            .setAuthor("You'r profile")
             .setColor("BLUE")
             .addField("Balance:", `€{userB}`);
             message.channel.send(emb)
