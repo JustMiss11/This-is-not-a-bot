849ef93359fc1de88aa2462fcd39c42ece5fa01b
JustMiss11 <47405121+JustMiss11@users.noreply.github.com>
2019-05-18 20:15:12 +0200

    Update hug.js

--- commands/hug.js
+++ commands/hug.js
@@ -2,14 +2,14 @@ const Discord = require('discord.js'),
       db = require('quick.db');
 module.exports.run = async (server, message, args) => {
       var user = message.mentions.members.first();
-      if(!user) return message.reply("is alone he wants some hug :)");
-      if(user.id === message.author.id) return message.reply("is feeling alone he wants some hugs ;)");
+      if(!user) return message.reply("is alone he/she wants some hug :)");
+      if(user.id === message.author.id) return message.reply("is feeling alone he/she wants some hugs ;)");
       db.add(`hugsG_${message.author.id}`, 1)
       db.add(`hugsT_${user.id}`, 1)
       var embed = new Discord.RichEmbed()
       .setAuthor(message.author.username)
       .setColor("GREEN")
-      .addField(`Hugged`, `<@{user.id}>`)
+      .addField(`Hugged`, `<@${user.id}>`)
       .addField("Its a nice moment :)", "Cuz everyone want hug!");
       message.channel.send(embed);
       return;
