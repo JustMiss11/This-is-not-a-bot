80a91e776d7038b4d3cc8c704ac074456a1896b3
JustMiss11 <47405121+JustMiss11@users.noreply.github.com>
2019-05-19 12:16:43 +0200

    Create donate.js

---
+++ commands/donate.js
@@ -0,0 +1,12 @@
+const Discord = require("discord.js");
+module.exports.run = async (server, message, args) => {
+      var emb = new Discord.RichEmbed()
+      .setAuthor("Donation", message.guild.iconURL)
+      .addField("Patreon", "[Donate me tought patreon here](https://www.patreon.com/user?u=19614687)")
+      .addField("PayPal", "enter gmail: `redsrafter@gmail.com` Im not very active on that gmail so dont try to send there random things.")
+      .setColor("BLUE");
+      message.author.send(emb);
+}
+module.exports.help = {
+  name: "donate"
+}
