15cb6cb1b8780d0d1c2f5dd29772e168b9f3b118
JustMiss11 <47405121+JustMiss11@users.noreply.github.com>
2019-05-19 12:16:49 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -6,10 +6,13 @@ module.exports.run = async (server, message, args) => {
   .setColor("0x36393f")
   .setTimestamp()
   .addField("say", "Say command will say what you want to say xd")
-  .addField("set", "```prefix <prefix> \nreports <channel>```")
+  .addField("set", "```prefix <prefix> \nreports <channel> \nstatus <status>```")
   .addField("report-bug", "Reports bug to the support server")
   .addField("icon", "Shows server icon for 5 seconds.")
   .addField("support", "Gives invite to support server.")
+  .addField("profile", "Shows your profile")
+  .addField("buy", "Buy something from our shop.")
+  .addField("donate", "Donate the bot owner")
   .setDescription("`help` - Viewing.");
   message.channel.send(emb)
   return;
